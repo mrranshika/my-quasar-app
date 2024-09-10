227b16d430210f1d54ba905ba69197f40a046f36
mrranshika <mrranshika@gmail.com>
2024-09-10 22:29:18 +0530

    Modified some

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -1,22 +1,15 @@
 <template>
-  <q-item
-    clickable
-    tag="a"
-    target="_blank"
-    :href="props.link"
-  >
-    <q-item-section
-      v-if="props.icon"
-      avatar
-    >
-      <q-icon :name="props.icon" />
-    </q-item-section>
+<q-item clickable tag="router-link" :to="props.link">
+  <q-item-section v-if="props.icon" avatar>
+    <q-icon :name="props.icon" />
+  </q-item-section>
 
-    <q-item-section>
-      <q-item-label>{{ props.title }}</q-item-label>
-      <q-item-label caption>{{ props.caption }}</q-item-label>
-    </q-item-section>
-  </q-item>
+  <q-item-section>
+    <q-item-label>{{ props.title }}</q-item-label>
+    <q-item-label caption>{{ props.caption }}</q-item-label>
+  </q-item-section>
+</q-item>
+
 </template>
 
 <script setup>
